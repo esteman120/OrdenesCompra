d1fc7ce7d80f4ca8a28ec3c53f6261b36c57507c
Egojose <josemicrobiologo@gmail.com>
2019-08-29 10:45:07 -0500

    se agrega validación para fecha de entrega

--- src/app/generar-orden-compra/generar-orden-compra.component.ts
+++ src/app/generar-orden-compra/generar-orden-compra.component.ts
@@ -447,6 +447,15 @@ export class GenerarOrdenCompraComponent implements OnInit {
 
   }
 
+  validarFecha() {
+    let fecha1 = this.generarOrdenForm.get('FechaSolicitud').value;
+    let fecha2 = this.generarOrdenForm.get('TiempoEntrega').value;
+    if(fecha1 > fecha2) {
+      this.mostrarAdvertencia('La fecha de solicitud no puede ser posterior a la fecha de entrega')
+      this.generarOrdenForm.controls['TiempoEntrega'].setValue("");
+    }
+  }
+
   calcularIva() {
     this.Subtotal = 0;
     this.ItemsGuardar.map(x=>{
